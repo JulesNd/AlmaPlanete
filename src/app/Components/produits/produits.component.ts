6fa965ad8488a863449ef17d7e4bedad4faa06b8
JulesxD <julesfran@outlook.fr>
2020-06-17 17:51:53 +0200

    Add existing file

--- src/app/Components/produits/produits.component.ts
+++ src/app/Components/produits/produits.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import {Observable} from 'rxjs';
+import {Post} from '../posts';
+import {PostService} from '../admin/post.service';
+import {AuthService} from '../core/auth.service';
 
 @Component({
   selector: 'app-produits',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produits.component.css']
 })
 export class ProduitsComponent implements OnInit {
+  email: string;
 
-  constructor() { }
+  password: string;
+
+  user: firebase.User;
+  posts: Observable<Post[]>;
+  constructor(private postService: PostService, private auth: AuthService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.auth.getUserState().subscribe(user => {this.user = user; });
+    this.auth.eventAuthError$.subscribe(data => {
+      console.log(this);
+    });
   }
 
 }
