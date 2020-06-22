dbfed0c56375c0c96d050d23fff914ec92048a75
JulesxD <julesfran@outlook.fr>
2020-06-22 14:32:07 +0200

    add file

--- src/app/Components/produits/produits.component.ts
+++ src/app/Components/produits/produits.component.ts
@@ -25,6 +25,15 @@ export class ProduitsComponent implements OnInit {
     this.auth.eventAuthError$.subscribe(data => {
       console.log(this);
     });
+    // tslint:disable-next-line:prefer-const
+    let autoPlayVideo = document.getElementById('video-element') as HTMLVideoElement  ;
+    // tslint:disable-next-line:only-arrow-functions
+    autoPlayVideo.oncanplaythrough = function() {
+      autoPlayVideo.muted = true;
+      autoPlayVideo.play();
+      autoPlayVideo.pause();
+      autoPlayVideo.play();
+    };
   }
 
 }
