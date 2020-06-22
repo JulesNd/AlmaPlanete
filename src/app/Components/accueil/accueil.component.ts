a8f6159c3b4e0901f8e637f6c08cde17666f4a88
adossantosalfam <anthony.dos_santos_alfama@insa-rouen.fr>
2020-06-22 16:39:51 +0200

    Modification page Nos Choix

--- src/app/Components/accueil/accueil.component.ts
+++ src/app/Components/accueil/accueil.component.ts
@@ -10,7 +10,7 @@ export class AccueilComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $("#zebraFirst").mouseover(function(){this.src="https://drive.google.com/uc?export=view&id=1m_K9S5qMt207mIb5rR5fY_fQxcQ4GDwA"}).mouseout(function(){this.src="https://drive.google.com/uc?export=view&id=1QfdPtPJLEwVn69PwnrZ6j4crO1fimqWR"});
+    
   }
 
 }
